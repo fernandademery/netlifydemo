89d68b46061b72a5d4b84d2183c28e1ce2445c0c
fernandademery <nandademery2@hotmail.com>
2022-09-21 14:10:56 +0200

    Restore repo

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/visit kiwigrid/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
