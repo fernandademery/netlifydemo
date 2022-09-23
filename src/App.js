4cf1e49049ba6e3a3ec696ea690168f28754d712
fernandademery <nandademery2@hotmail.com>
2022-09-23 14:18:14 +0200

    change content

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://kiwigrid.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Kiwigrid
         </a>
       </header>
     </div>
